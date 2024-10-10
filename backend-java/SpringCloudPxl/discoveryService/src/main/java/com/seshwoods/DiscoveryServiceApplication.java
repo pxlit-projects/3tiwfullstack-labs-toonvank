804565fc542542dd7b7faad8fbffb55a7ec53a6e
Toon <toonvankimmenade@gmail.com>
2024-10-10 13:47:26 +0200

    Lab 3 & 4

---
+++ backend-java/SpringCloudPxl/discoveryService/src/main/java/com/seshwoods/DiscoveryServiceApplication.java
@@ -0,0 +1,18 @@
+package com.seshwoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Discovery Service
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class);
+    }
+}
